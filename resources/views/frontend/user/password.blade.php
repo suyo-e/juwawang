31ec8b00d2172b350215909936c79cdb00add5b3
billqiang <billqiang@qq.com>
2017-03-28 18:09:14 +0800

    password

--- resources/views/frontend/user/password.blade.php
+++ resources/views/frontend/user/password.blade.php
@@ -1,14 +1,14 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-{!! Form::open(['route' => 'frontend.users.update']) !!}
 <div class="modify" style="text-align:center">
-    <label><input class="inputAll" type="number" placeholder=" 原始密码" value="{{old('old_password')}}" name="old_password"></label>
-    <label><input class="inputAll" type="password" placeholder=" 请输入新密码" value="{{old('new_password')}}" name="new_password"></label>
-    <label><input class="inputAll" type="password" placeholder=" 请再次输入新密码" value="{{old('rep_password')}}" name="rep_password"></label>
-    <label><input class="btnAll" type="submit" value="确认修改"></label>
-</div>
+{!! Form::open(['route' => 'frontend.users.update']) !!}
+    <input class="inputAll" type="number" placeholder=" 原始密码" value="{{old('old_password')}}" name="old_password">
+    <input class="inputAll" type="password" placeholder=" 请输入新密码" value="{{old('new_password')}}" name="new_password">
+    <input class="inputAll" type="password" placeholder=" 请再次输入新密码" value="{{old('rep_password')}}" name="rep_password">
+    <input class="btnAll" type="submit" value="确认修改">
 {!! Form::close() !!}
+</div>
 
 @endsection
 
